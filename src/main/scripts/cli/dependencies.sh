239524f14a00b7a6b36fbd0983586f2573b726ef
ssimard <ssimard@pasteur.fr>
2014-01-21 10:11:39 +0100

    Prepare for point release 0.2.1-SNAPSHOT

--- src/main/scripts/cli/dependencies.sh
+++ src/main/scripts/cli/dependencies.sh
@@ -8,7 +8,7 @@
 # Distribution unpack directory
 # Assumed to be the current folder for the executed script
 JARS_DIR=`dirname $0`
-CSV_TOOL_VERSION="0.3-SNAPSHOT"
+CSV_TOOL_VERSION="0.2.1-SNAPSHOT"
 
 ####
 # Dependencies provided by the "with-dependencies" distribution 
