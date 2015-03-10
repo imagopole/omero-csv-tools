9cc8cf749e7afadd6ba6c6b89f3b0c7345608f85
ssimard <ssimard@pasteur.fr>
2015-03-10 18:39:21 +0100

    Version freeze from 0.2.4-SNAPSHOT to 0.3

--- src/main/scripts/omero/annotation_scripts/Csv_Annotation_Tool.py
+++ src/main/scripts/omero/annotation_scripts/Csv_Annotation_Tool.py
@@ -400,7 +400,7 @@ def run_as_script():
         ),
 
     #---- script metadata
-    version      = "0.2.3 [OMERO-5.0.x-ice34]",
+    version      = "0.3 [OMERO-5.0.x-ice34]",
     authors      = [ "S. Simard", "Imagopole" ],
     institutions = [ "Institut Pasteur" ],
     contact      = "ssimard@pasteur.fr"
