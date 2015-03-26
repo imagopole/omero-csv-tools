f1996de5812451bff41501e0cfd15d5cbfd97168
ssimard <ssimard@pasteur.fr>
2015-03-26 11:17:01 +0100

    Enable csv file name tokens substitution in cli args

--- src/main/java/org/imagopole/omero/tools/api/cli/CsvAnnotationConfig.java
+++ src/main/java/org/imagopole/omero/tools/api/cli/CsvAnnotationConfig.java
@@ -5,13 +5,14 @@ package org.imagopole.omero.tools.api.cli;
 
 import static org.imagopole.omero.tools.util.ParseUtil.empty;
 
-import com.google.common.base.Objects;
-
 import org.imagopole.omero.tools.api.cli.Args.AnnotatedType;
+import org.imagopole.omero.tools.api.cli.Args.AnnotationType;
 import org.imagopole.omero.tools.api.cli.Args.ContainerType;
 import org.imagopole.omero.tools.api.cli.Args.Defaults;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Objects;
 
 /**
  * Configuration settings for the CSV Annotation Tool.
@@ -60,15 +61,39 @@ public class CsvAnnotationConfig {
         // get CLI argument as configured if available
         String configuredFilename = getCsvFileName();
 
+        ContainerType containerType = ContainerType.valueOf(getCsvContainerTypeArg());
+        AnnotationType annotationType = AnnotationType.valueOf(getAnnotationTypeArg());
+        AnnotatedType annotatedType = getOrInferEffectiveAnnotatedType();
+
         if (null != configuredFilename && ! configuredFilename.isEmpty()) {
             log.debug("Reusing csv filename from configured argument: {}", configuredFilename);
+
+            // perform tokens substitution if needed
+            boolean shouldExpandTokens =
+                configuredFilename.matches(NamingConventions.Patterns.HAS_TOKENS);
+
+            if (shouldExpandTokens) {
+
+                String expandedFileName =
+                    NamingConventions.expandTokens(
+                        configuredFilename,
+                        containerType,
+                        getContainerId(),
+                        annotationType,
+                        annotatedType,
+                        getExportMode());
+
+                log.debug("Tokens expansion in csv filename argument: {}", expandedFileName);
+                return expandedFileName;
+            }
+
             return configuredFilename;
         }
 
         // otherwise use the naming convention (based on the _effective_ annotation target)
         String inferredFileName =
-            NamingConventions.build(
-                getOrInferEffectiveAnnotatedType().name(), getAnnotationTypeArg(), getExportMode());
+            NamingConventions.buildFullName(annotatedType, annotationType, getExportMode());
+
         log.info("Inferring csv filename from container & annotation types: {}", inferredFileName);
 
         return inferredFileName;
@@ -124,12 +149,14 @@ public class CsvAnnotationConfig {
     /**
      * FileAnnotation naming convention to hold tagging requests in CSV files.
      *
-     * As follows: <annotatedType>_<annotationType>.csv (annotation mode)
-     *             <annotatedType>_<annotationType>.export.csv (export mode)
+     * <pre>
+     * As follows: {@code <annotatedType>_<annotationType>.csv}        (annotation mode)
+     *             {@code <annotatedType>_<annotationType>.export.csv} (export mode)
      * Eg.
      *   - dataset_tag.csv     | dataset_tag.export.csv     : tags applied to datasets
      *   - dataset_comment.csv | dataset_comment.export.csv : comments applied to datasets
      *   - image_tag.csv       | image_tag.export.csv       : tags applied to images
+     * </pre>
      */
     private static final class NamingConventions {
 
@@ -138,16 +165,79 @@ public class CsvAnnotationConfig {
         private static final String EXTENSION = ".csv";
         private static final String EXPORT_QUALIFIER = ".export";
 
-        private static final String build(String containerType, String annotationType) {
-            return PREFIX + containerType + SEPARATOR + annotationType + EXTENSION;
+        /**
+         * Regexp patterns to used for well-known tokens substitution within the csv filenames.
+         *
+         * Tokens are in the form of: {@code <token>}, with both a shorthand and expanded token
+         * available (eg.  {@code <cid>} and {@code <container-id>}).
+         */
+        private static final class Patterns {
+
+            private static final String HAS_TOKENS        = ".*<.*>.*";
+            private static final String CONTAINER_ID      = "<cid>|<container-id>";
+            private static final String CONTAINER_TYPE    = "<ctype>|<container-type>";
+            private static final String ANNOTATION_TYPE   = "<antype>|<annotation-type>";
+            private static final String ANNOTATED_TYPE    = "<antarget>|<annotated-type>";
+
+            /** "Meta" pattern equivalent to {@code <annotated-type>_<annotation-type>}. */
+            private static final String DEFAULT_BASENAME  = "<basename>";
+
+            /** Expands to {@code .csv} or {@code .export.csv}. */
+            private static final String DEFAULT_SUFFIX    = "<suffix>";
+
+            /**
+             * Constants class.
+             */
+            private Patterns() {
+                super();
+            }
         }
 
-        private static final String build(String containerType, String annotationType, Boolean isExportMode) {
+        private static final String buildBasename(
+                AnnotatedType annotatedType, AnnotationType annotationType) {
+
+            return PREFIX + annotatedType + SEPARATOR + annotationType;
+        }
+
+        private static String buildSuffix(Boolean isExportMode) {
             if (null == isExportMode || !isExportMode) {
-                return build(containerType, annotationType);
+                return EXTENSION;
             }
 
-            return PREFIX + containerType + SEPARATOR + annotationType + EXPORT_QUALIFIER + EXTENSION;
+            return EXPORT_QUALIFIER.concat(EXTENSION);
+        }
+
+        private static final String buildFullName(
+                AnnotatedType annotatedType, AnnotationType annotationType, Boolean isExportMode) {
+
+           return buildBasename(annotatedType, annotationType).concat(buildSuffix(isExportMode));
+        }
+
+        /**
+         * Perform substitution for well-known filename tokens.
+         *
+         * @param configuredFilename the input filename with token patterns
+         * @return the expanded filename
+         */
+        private static String expandTokens(
+            String configuredFilename,
+            ContainerType containerType,
+            Long containerId,
+            AnnotationType annotationType,
+            AnnotatedType annotatedType,
+            Boolean isExportMode) {
+
+            String result =
+                configuredFilename
+                    .trim()
+                    .replaceAll(Patterns.DEFAULT_BASENAME, buildBasename(annotatedType, annotationType))
+                    .replaceAll(Patterns.DEFAULT_SUFFIX, buildSuffix(isExportMode))
+                    .replaceAll(Patterns.CONTAINER_ID, containerId.toString())
+                    .replaceAll(Patterns.CONTAINER_TYPE, containerType.name())
+                    .replaceAll(Patterns.ANNOTATION_TYPE, annotationType.name())
+                    .replaceAll(Patterns.ANNOTATED_TYPE, annotatedType.name());
+
+            return result;
         }
 
         private NamingConventions() {
